c23c2ff5a57ee4ad6cc54c1e1ea59cbd4d77f86a
Jan Kiesewetter <233696281+jank13@users.noreply.github.com>
2025-09-22 21:55:16 +0200

    Add sitemap integration and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,22 @@
 // @ts-check
 
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: 'de',
+        locales: {
+          de: 'de-DE',
+          en: 'en-US'
+        }
+      }
+    })
+  ],
   site: 'https://kwetter-de.github.io',
   vite: {
     plugins: [tailwindcss()]
